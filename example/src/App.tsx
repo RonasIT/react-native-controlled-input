6c007c6fbc13b0295634eb4df58071c288e1228a
Василий Елисеев <veliseev@ronasit.com>
2026-04-22 11:23:14 +0600

    chore: example app update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,7 @@ import {
   type ControlledInputViewRef,
 } from '@ronas-it/react-native-controlled-input';
 import { useRef, useState, type ReactElement } from 'react';
-import { StyleSheet, ScrollView, Button, View, TextInput } from 'react-native';
+import { StyleSheet, ScrollView, Button, View } from 'react-native';
 
 const formatPromoCode = (input: string): string => {
   let letters = '';
@@ -134,7 +134,7 @@ export default function App(): ReactElement {
           console.log('onSubmitEditing');
         }}
       />
-      
+
       <Button title='Focus' onPress={focus} />
       <Button title='Blur' onPress={blur} />
 
